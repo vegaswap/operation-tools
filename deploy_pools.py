665e3f23eeb35844c8cf8c8668866e43d460e71f
benjyz <ben@vegaswap.io>
2021-11-08 16:49:06 +0700

    proper script

---
+++ deploy_pools.py
@@ -0,0 +1,125 @@
+from brownie import VegaToken, BoostPool, accounts, network, project
+from brownie import web3
+
+BSC_USDT = "0x55d398326f99059ff775485246999027b3197955"
+VGA_CTR = "0x4EfDFe8fFAfF109451Fc306e0B529B088597dd8d"
+
+from pathlib import Path
+import os
+
+#TODO review: reward steps and stake steps have different length
+
+def deploy_usdt(mainaccount):    
+    
+    _stakeToken = BSC_USDT
+    _yieldToken = VGA_CTR
+    hour = 60*60
+    day = 24 * hour    
+    _duration = 7 * day
+    f = 10 ** 18
+    #TODO
+    _maxYield = 300000 * f
+    _maxTotalStake = 25000 * f
+    _maxPerStake = 1000 * f
+    _rewardSteps = [20, 15, 12, 8, 5]
+    xstep = 5000
+    _stakeSteps = [xstep * f, (xstep*2) * f, (xstep*3) * f, (xstep*4) * f]
+    rewardQuote = 1
+
+    #7pm
+    _startTime = 1636372800
+
+    bpool = BoostPool.deploy(
+        _startTime,
+        _duration,
+        _stakeToken,
+        _yieldToken,
+        _maxYield,
+        _maxTotalStake,
+        _maxPerStake,
+        _rewardSteps,
+        _stakeSteps,
+        rewardQuote,
+        {"from": mainaccount},
+    )
+    print(bpool)    
+    print("deployed USDT pool", bpool)
+
+
+def deploy_vga(mainaccount):
+    
+    vega = VegaToken.at(VGA_CTR)
+    bv = vega.balanceOf(mainaccount)
+    print("Vega ", bv / 10 ** 18)
+
+    usdt = VegaToken.at(BSC_USDT)
+    b = usdt.balanceOf(mainaccount)
+    usdt_dec = 18
+    print("USDT balance ", b / 10 ** usdt_dec)
+
+    bnb = network.web3.eth.get_balance(mainaccount.address)
+    print(bnb / 10 ** 18)
+
+    _stakeToken = VGA_CTR
+    _yieldToken = VGA_CTR
+    hour = 60*60
+    day = 24 * hour
+    #_duration = hour * 6
+    _duration = 7 * day
+    f = 10 ** 18
+    #TODO
+    _maxYield = 325000 * f
+    _maxTotalStake = 2500000 * f
+    _maxPerStake = 5000 * f
+    #_minStake = 10 * f
+    
+    _rewardSteps = [20, 15, 12, 8, 5]
+    xstep = 500000
+    _stakeSteps = [xstep * f, (xstep*2) * f, (xstep*3) * f, (xstep*4) * f]
+    rewardQuote = 100
+
+    i = 0
+    totalr = 0
+    for x in _stakeSteps:
+        r = _rewardSteps[i]
+        rw = r * x
+        print(rw)
+        i+=1
+        totalr += rw
+    print("total ", totalr/10**18)
+
+    #7pm
+    _startTime = 1636372800
+
+    bpool = BoostPool.deploy(
+        _startTime,
+        _duration,
+        _stakeToken,
+        _yieldToken,
+        _maxYield,
+        _maxTotalStake,
+        _maxPerStake,
+        #$_minStake,
+        _rewardSteps,
+        _stakeSteps,
+        rewardQuote,
+        {"from": mainaccount},
+    )
+    print("deployed VGA pool", bpool)
+
+def main():
+
+    net = network.show_active()
+    # network.disconnect()
+    # network.connect("bscmain")
+    if net == "bscmain":    
+        accounts.add(os.environ["PRIVKEY"])
+        mainaccount = accounts[0]
+        print(mainaccount)
+
+        # deploy_usdt(mainaccount)
+        # deploy_vga(mainaccount)
+
+
+
+
